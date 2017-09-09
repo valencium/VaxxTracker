a905be549aa62d51eef7be91c1fd8b7b56545f38
konaraddio <omkar.konaraddi@gmail.com>
2017-09-09 17:57:04 -0400

    added spoof data

--- js/account_info.js
+++ js/account_info.js
@@ -1,18 +1,2 @@
-window.onload= function(){
-    console.log("the javascript is loading");
-    var source = $("#template-row").html();
-    var template = Handlebars.compile(source);  
-    var data = { 
-        vaccinations: [
-            {
-                name: "FLU234",
-                occurrence:["0", "1", "0", "1", "0"]
-            },
-            {
-                name: "FLU234",
-                occurrence:["0", "1", "0", "1", "0"]
-            }
-        ]
-    };
-    $("#vaccination-rows").html(template(data));
-};
+
+
